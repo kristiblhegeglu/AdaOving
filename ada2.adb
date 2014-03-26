3f956df189bbd1d020d6576ac15098dc3c301c5d
kristiblhegeglu <kristibl@stud.ntnu.no>
2014-03-26 09:46:35 +0100

    Update ada2.adb

--- ada2.adb
+++ ada2.adb
@@ -4,101 +4,129 @@
  --- (Ada tabs = 3 spaces)
  -
  -procedure ada2 is
- -
- -   Count_Failed : exception; -- Exception to be raised when counting fails
- -   Gen : Generator; -- Random number generator
- -
- -   protected type Transaction_Manager (N : Positive) is
- -      entry Finished;
- -      function Commit return Boolean;
- -      procedure Signal_Abort;
- -   private
- -      Finished_Gate_Open : Boolean := False;
- -      Aborted : Boolean := False;
- -      Will_Commit : Boolean := True;
- -   end Transaction_Manager;
- -   protected body Transaction_Manager is
- -      entry Finished when Finished_Gate_Open or Finished'Count = N is
- -      begin
- -         ------------------------------------------
- -         -- PART 3: Complete the exit protocol here
- -         ------------------------------------------
- -         
- -         
- -         
- -      end Finished;
- -
- -      procedure Signal_Abort is
- -      begin
- -         Aborted := True;
- -      end Signal_Abort;
- -
- -      function Commit return Boolean is
- -      begin
- -         return Will_Commit;
- -      end Commit;
- -      
- -   end Transaction_Manager;
- -
- -
- -
- -   
- -   function Unreliable_Slow_Add (x : Integer) return Integer is
- -   Error_Rate : Constant := 0.15; -- (between 0 and 1)
- -   begin
- -      -------------------------------------------
- -      -- PART 1: Create the transaction work here
- -      -------------------------------------------
- -    
- -      
- -   end Unreliable_Slow_Add;
- -
- -
- -
- -
- -   task type Transaction_Worker (Initial : Integer; Manager : access Transaction_Manager);
- -   task body Transaction_Worker is
- -      Num : Integer := Initial;
- -      Prev : Integer := Num;
- -      Round_Num : Integer := 0;
- -   begin
- -      Put_Line ("Worker" & Integer'Image(Initial) & " started");
- -
- -      loop
- -         Put_Line ("Worker" & Integer'Image(Initial) & " started round" & Integer'Image(Round_Num));
- -         Round_Num := Round_Num + 1;
- -
- -         ---------------------------------------
- -         -- PART 2: Do the transaction work here
- -         ---------------------------------------
- -         
- -         
- -         Manager.Finished;
- -         
- -         if Manager.Commit = True then
- -            Put_Line (" Worker" & Integer'Image(Initial) & " comitting" & Integer'Image(Num));
- -         else
- -            Put_Line (" Worker" & Integer'Image(Initial) &
- -                      " reverting from" & Integer'Image(Num) &
- -                      " to" & Integer'Image(Prev));
- -            -------------------------------------------
- -            -- PART 2: Roll back to previous value here
- -            -------------------------------------------
- -            
- -         end if;
- -
- -         Prev := Num;
- -         delay 0.5;
- -
- -      end loop;
- -   end Transaction_Worker;
- -
- -   Manager : aliased Transaction_Manager (3);
- -
- -   Worker_1 : Transaction_Worker (0, Manager'Access);
- -   Worker_2 : Transaction_Worker (1, Manager'Access);
- -   Worker_3 : Transaction_Worker (2, Manager'Access);
- -
- -begin
- -   Reset(Gen); -- Seed the random number generator
+     -- Wait_Until_Aborted : exception;
+   Count_Failed : exception; -- Exception to be raised when counting fails
+   Gen : Generator; -- Random number generator
+
+   protected type Transaction_Manager (N : Positive) is
+      entry Finished;
+      entry Wait_Until_Aborted;
+-- function Commit return Boolean;
+      procedure Signal_Abort;
+   private
+      Finished_Gate_Open : Boolean := False;
+      Aborted : Boolean := False;
+      
+   end Transaction_Manager;
+   protected body Transaction_Manager is
+      entry Finished when Finished_Gate_Open or Finished'Count = N is
+      begin
+         ------------------------------------------
+         -- PART 3: Complete the exit protocol here
+         ------------------------------------------
+         if Finished'Count=N-1 then
+            Finished_Gate_open:= true;
+         end if;
+         if Finished'Count=0 then
+         Finished_Gate_open:=false;
+         end if;
+      end Finished;
+
+      procedure Signal_Abort is
+      begin
+         Aborted := True;
+
+      end Signal_Abort;
+
+      entry Wait_Until_Aborted when Aborted is
+      begin
+         if Wait_Until_Aborted'Count=0 then
+            Aborted:=false;
+         end if;
+      end Wait_Until_Aborted;
+-- function Commit return Boolean is
+ -- begin
+-- return Will_Commit;
+-- end Commit;
+      
+   end Transaction_Manager;
+
+
+
+   
+   function Unreliable_Slow_Add (x : Integer) return Integer is
+   Error_Rate : Constant := 0.15; -- (between 0 and 1)
+   num : integer :=10;
+   begin
+      -------------------------------------------
+      -- PART 1: Create the transaction work here
+      -------------------------------------------
+        case Random(Gen)>Error_Rate is
+         when true =>
+            delay Duration(Random(Gen)*4.2);
+            num:=x+10;
+         when false =>
+            delay Duration(Random(Gen)*0.6);
+            raise Count_Failed with "Failed_slow_add";
+      end case;
+      return num;
+   end Unreliable_Slow_Add;
+
+
+
+
+   task type Transaction_Worker (Initial : Integer; Manager : access Transaction_Manager);
+   task body Transaction_Worker is
+      Num : Integer := Initial;
+      Prev : Integer := Num;
+      Round_Num : Integer := 0;
+   begin
+      Put_Line ("Worker" & Integer'Image(Initial) & " started");
+
+      loop
+         Put_Line ("Worker" & Integer'Image(Initial) & " started round" & Integer'Image(Round_Num));
+         Round_Num := Round_Num + 1;
+
+         ---------------------------------------
+         -- PART 2: Do the transaction work here
+         ---------------------------------------
+         select
+            Manager.Wait_Until_Aborted;
+            Num:=Prev+5;
+            Put_Line("aborted, adding 5, num="& Integer'Image(Num));
+         then abort
+         begin
+            Num:=Unreliable_Slow_Add(Prev);
+         exception
+            when Count_Failed=>
+               Manager.Signal_Abort;
+         end;
+         end select;
+         Manager.Finished;
+         Put_Line("All went well, num="& Integer'Image(Num));
+         
+         
+ -- if Manager.Commit = True then
+   -- Put_Line (" Worker" & Integer'Image(Initial) & " comitting" & Integer'Image(Num));
+    -- else
+      -- Put_Line (" Worker" & Integer'Image(Initial) &
+        -- " reverting from" & Integer'Image(Num) &
+          -- " to" & Integer'Image(Prev));
+
+     -- end if;
+            
+         Prev := Num;
+         delay 0.5;
+
+      end loop;
+   end Transaction_Worker;
+
+   Manager : aliased Transaction_Manager (3);
+
+   Worker_1 : Transaction_Worker (0, Manager'Access);
+   Worker_2 : Transaction_Worker (1, Manager'Access);
+   Worker_3 : Transaction_Worker (2, Manager'Access);
+
+begin
+   Reset(Gen); -- Seed the random number generator
  -end ada2;
